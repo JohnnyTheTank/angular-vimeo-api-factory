f45883dea8c46862e13a714072293adf0e2528b3
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-09 20:31:07 +0100

    demo: updated demo

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,42 +1,38 @@
 var app = angular.module("app", ['jtt_vimeo']);
 app.controller('controller', ['$scope', 'vimeoFactory', function($scope, vimeoFactory) {
 
-    var _acces_token = "<YOUR_VIMEO_ACCESS_TOKEN>";
+    var _access_token = "<YOUR_VIMEO_ACCESS_TOKEN>";
 
     vimeoFactory.getVideosFromChannel({
         channel:"nicetype",
         per_page:2,
-        access_token:_acces_token
+        access_token:_access_token
     }).success(function(_data){
-        console.log("videos from channel");
-        console.log(_data);
+        console.info("videos from channel", _data);
     });
 
     vimeoFactory.getVideosFromCategory({
         category:"cameratechniques",
         per_page:2,
-        access_token:_acces_token
+        access_token:_access_token
     }).success(function(_data){
-        console.log("videos from category");
-        console.log(_data);
+        console.info("videos from category", _data);
     });
 
     vimeoFactory.getVideosFromTag({
         tag:"fcbayern",
         per_page:2,
-        access_token:_acces_token
+        access_token:_access_token
     }).success(function(_data){
-        console.log("videos from tag");
-        console.log(_data);
+        console.info("videos from tag", _data);
     });
 
     vimeoFactory.getVideosFromUser({
         user:"alexanderengel",
         per_page:2,
-        access_token:_acces_token
+        access_token:_access_token
     }).success(function(_data){
-        console.log("videos from user");
-        console.log(_data);
+        console.info("videos from user", _data);
     });
 
 }]);
